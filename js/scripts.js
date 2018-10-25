66412a5fb155e06fd0caac96eeba185b9b1eb5ca
Katrina Hockman and Paige Williams <paige@>
2018-10-25 12:03:21 -0700

    Add function to repeat roman numeral based off of remainder when divided by 10.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 //business logic
 var symbols = ["I", "V", "X", "L", "C", "D", "M"];
 var repeatI = "I";
+// var numeral123 = [];
 
 var valueToSymbol = function(number) {
   if (number > 0 && number < 4){
+   var numeral123 = repeatI.repeat(number);
 
-    var numeral123 = repeatI.repeat(number);
     return numeral123;
+    console.log(numeral123)
   }
+  // if (number === 4) {
+  //       var numeral4 = [];
+  //     numeral4.push(numeral123.slpit("")  numeral123.slice(2) + "V")
+  //
+  //       return numeral4;
+
+
   // if (number === 5){
   //   return "V";
   // }
@@ -15,13 +24,19 @@ var valueToSymbol = function(number) {
 
     var remainder = number % 5;
     var numeral678 =  "V" + (repeatI.repeat(remainder));
+
     return numeral678;
   }
-  // if (number === 10){
-  //   return "V";
-  // }
+  if (number === 10){
+    return "X";
+  }
+  if (number >= 10 && number < 13){
 
+    var remainder = number % 10;
+    var numeral13 =  "X" + (repeatI.repeat(remainder));
 
+    return numeral13;
+  }
 
 
 };
@@ -33,7 +48,7 @@ $(document).ready(function(){
     var number = parseInt($("#number").val());
 
     result = valueToSymbol(number);
-    console.log(result);
+    console.log(result)
 
     // debugger
   });
