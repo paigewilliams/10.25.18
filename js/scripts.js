9526a10c842d267d3811582d57041d005b6acead
Katrina Hockman and Paige Williams <paige@>
2018-10-25 14:22:04 -0700

    Add conversions fo rnumbers divisibl by 10

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
 //business logic
 var symbols = ["I", "V", "X", "L", "C", "D", "M"];
 var repeatI = "I";
+var repeatX = "X";
 // var numeral123 = [];
 
 var valueToSymbol = function(number) {
+  if (number < 40 && number % 10==0) {
+    var quotient = number / 10
+    var numeral10s =  repeatX.repeat(quotient);
+    return numeral10s;
+    console.log(numeral10s)
+    }
+
   if (number > 0 && number < 4){
    var numeral123 = repeatI.repeat(number);
 
     return numeral123;
     console.log(numeral123)
   }
-  // if (number === 4) {
-  //       var numeral4 = [];
-  //     numeral4.push(numeral123.slpit("")  numeral123.slice(2) + "V")
-  //
-  //       return numeral4;
 
-
-  // if (number === 5){
-  //   return "V";
-  // }
   if (number >= 5 && number < 9){
 
     var remainder = number % 5;
@@ -27,18 +26,23 @@ var valueToSymbol = function(number) {
 
     return numeral678;
   }
-  if (number === 10){
-    return "X";
-  }
+
   if (number >= 10 && number < 13){
 
     var remainder = number % 10;
-    var numeral13 =  "X" + (repeatI.repeat(remainder));
+    var numeral12 =  "X" + (repeatI.repeat(remainder));
 
-    return numeral13;
+    return numeral12;
   }
+  if (number === 4){
+    return "IV";
+  }
+  if (number === 9){
+    return "IX";
+    }
+//for (var i= 0; i < length; i++) {
 
-
+  //}
 };
 
 //user interface logic
