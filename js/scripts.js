7f73280828b73979832f86c7ac2bcf5f2152f066
Katrina Hockman and Paige Williams <paige@>
2018-10-25 14:45:06 -0700

    Add 1000s

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,40 @@
 var symbols = ["I", "V", "X", "L", "C", "D", "M"];
 var repeatI = "I";
 var repeatX = "X";
+var repeatC = "C";
+var repeatM = "M";
 // var numeral123 = [];
 
 var valueToSymbol = function(number) {
-  if (number < 40 && number % 10==0) {
-    var quotient = number / 10
-    var numeral10s =  repeatX.repeat(quotient);
-    return numeral10s;
-    console.log(numeral10s)
+  if (number < 4000 && number % 10==0) {
+    var quotient = number / 1000
+    var numeral1000s =  repeatM.repeat(quotient);
+    return numeral1000s;
+  }
+
+
+  if (number < 400 && number % 10==0) {
+    var quotient = number / 100
+    var numeral100s =  repeatC.repeat(quotient);
+    return numeral100s;
+
     }
+
+    if (number < 40 && number % 10==0) {
+      var quotient = number / 10
+      var numeral10s =  repeatX.repeat(quotient);
+      return numeral10s;
+
+    }
+if (number >10 && number <14)
+  var numeralTeen = "X" + repeatI.repeat(number);
+
 
   if (number > 0 && number < 4){
    var numeral123 = repeatI.repeat(number);
 
     return numeral123;
-    console.log(numeral123)
+
   }
 
   if (number >= 5 && number < 9){
